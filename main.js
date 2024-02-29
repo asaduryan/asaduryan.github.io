f4f39272aa8f305124d9788cb0ddef01fc50bc18
Asaduryan <asaduryan89@gmail.com>
2024-02-29 23:10:39 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = (event) => {
+  var paths = document.querySelectorAll('.st0');
+  [].forEach.call(paths, function (path) {
+    var length = path.getTotalLength();
+    path.style.transition = path.style.WebkitTransition = 'none';
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+  })
+
+  let namepath = document.querySelector(".title");
+  let length = namepath.getTotalLength();
+  namepath.style.strokeDasharray = length + ' ' + length;
+  namepath.style.strokeDashoffset = length;
+};
